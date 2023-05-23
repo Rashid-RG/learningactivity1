c9fc07dd05e447da49d752e448f57eb38bd6425f
Mohammed Rashid <105622061+Rashid-RG@users.noreply.github.com>
2023-05-23 19:12:01 +0530

    Add files via upload

---
+++ App1/App1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace App1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSum = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumbe1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(343, 252);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(87, 41);
+            this.btnSum.TabIndex = 0;
+            this.btnSum.Text = "SUM";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number1*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number2*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Result";
+            // 
+            // txtNumbe1
+            // 
+            this.txtNumbe1.Location = new System.Drawing.Point(310, 90);
+            this.txtNumbe1.Name = "txtNumbe1";
+            this.txtNumbe1.Size = new System.Drawing.Size(154, 26);
+            this.txtNumbe1.TabIndex = 4;
+            this.txtNumbe1.Text = "0";
+            this.txtNumbe1.Validating += new System.ComponentModel.CancelEventHandler(this.txtNumbe1_Validating);
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(310, 147);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(154, 26);
+            this.txtNumber2.TabIndex = 5;
+            this.txtNumber2.Text = "0";
+            this.txtNumber2.Validating += new System.ComponentModel.CancelEventHandler(this.txtNumber2_Validating);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(310, 194);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(154, 26);
+            this.txtResult.TabIndex = 6;
+            this.txtResult.Text = "0";
+            this.txtResult.Validating += new System.ComponentModel.CancelEventHandler(this.txtResult_Validating);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumbe1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumbe1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ App1/App1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App1
+{
+    public partial class Form1 : Form
+    {
+        // Variable Declare
+
+        private int number1;
+        private int number2;
+        private int result;
+        ErrorProvider errorProvider;
+
+
+        private int intValidation;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialise Variables on form constructor.
+            number1 = 0;
+            number2 = 0;
+            errorProvider = new ErrorProvider();
+
+            // change attribute
+
+            txtResult.ReadOnly = true;
+        }
+
+        private void txtNumbe1_Validating(object sender, CancelEventArgs e)
+        {
+            // Clear errorProvider
+
+
+            errorProvider.SetError(txtNumbe1, "");
+            if ( int.TryParse(txtNumbe1.Text, out intValidation))
+            { errorProvider.SetError(txtNumbe1, " please Fill the reqired field");
+            
+            }
+        }
+
+        private void txtNumber2_Validating(object sender, CancelEventArgs e)
+        {
+            errorProvider.SetError(txtNumber2, "");
+            if (int.TryParse(txtNumber2.Text, out intValidation))
+            {
+                errorProvider.SetError(txtNumber2, " please Fill the reqired field");
+
+            }
+        }
+
+        private void txtResult_Validating(object sender, CancelEventArgs e)
+        {
+            errorProvider.SetError(txtResult, "");
+            if (int.TryParse(txtResult.Text, out intValidation))
+            {
+                errorProvider.SetError(txtResult, " please Fill the reqired field");
+
+            }
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(ValidateChildren(ValidationConstraints.Enabled))
+                {
+                    number1 = int.Parse(txtNumbe1.Text);
+                    number2 = int.Parse(txtNumber2.Text);
+                    result = (number1 + number2);
+                    txtResult.Text = result.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
